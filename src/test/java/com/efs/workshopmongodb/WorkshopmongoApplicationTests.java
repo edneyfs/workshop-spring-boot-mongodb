8e7593fe810b630c1047dc45c725fba52c308e21
Edney Siqueira <edneyfs@gmail.com>
2020-09-17 18:07:49 -0300

    projeto criado

---
+++ src/test/java/com/efs/workshopmongodb/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efs.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
